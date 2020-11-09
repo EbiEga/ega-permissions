960c97970ddd761d6d35565be4842daebf3bb751
Emilio Garcia <egarcia@ebi.ac.uk>
2020-11-09 14:41:08 +0000

    EE-1593 Add timestamp column

---
+++ src/main/resources/db/migration/V7__Timestamp.sql
@@ -0,0 +1,31 @@
+-- Modify passport_claim table
+ALTER TABLE passport_claim
+ADD COLUMN "timestamp" timestamp DEFAULT now();
+
+-- Modify user_group table
+ALTER TABLE user_group
+ADD COLUMN "timestamp" timestamp DEFAULT now();
+
+-- Add function
+CREATE OR REPLACE FUNCTION fn_update_set_timestamp()
+  RETURNS TRIGGER
+  LANGUAGE PLPGSQL
+  as
+$$
+BEGIN
+	new.timestamp = now();
+	return new;
+END;
+$$;
+
+-- Add trigger to passport_claim
+CREATE TRIGGER on_update_passport_claim_set_timestamp
+    AFTER UPDATE ON passport_claim
+    FOR EACH ROW
+    EXECUTE function fn_update_set_timestamp();
+
+-- Add trigger to user_group
+CREATE TRIGGER on_update_user_group_set_timestamp
+    AFTER UPDATE ON user_group
+    FOR EACH ROW
+    EXECUTE function fn_update_set_timestamp();
