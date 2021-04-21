bba81eff79a196fd6313ba045ae089c0405b936e
anandmohan777 <anandmohan777@gmail.com>
2021-04-21 19:13:20 +0100

    changing query for table pea.account_elixir_id

--- src/main/resources/db/migration/V9__AccountElixirIdTimestamp.sql
+++ src/main/resources/db/migration/V9__AccountElixirIdTimestamp.sql
@@ -1,13 +1,4 @@
-IF NOT EXISTS( SELECT NULL
-            FROM INFORMATION_SCHEMA.COLUMNS
-           WHERE table_name = 'account_elixir_id'
-             AND table_schema = 'pea'
-             AND column_name = 'created_at')  THEN
-
-  ALTER TABLE pea.account_elixir_id ADD COLUMN created_at timestamp DEFAULT now();
-
-END IF;
-
+ALTER TABLE pea.account_elixir_id ADD COLUMN IF NOT EXISTS created_at timestamp DEFAULT now();
 
 CREATE OR REPLACE VIEW account_elixir_id
 AS SELECT ae.account_id,
