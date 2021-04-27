386c7a22138501e6dd44c455a9e9839028be34f8
egarcia@ebi.ac.uk <Emilio Garcia>
2021-04-27 22:43:13 +0100

    EE-1911 EE-1911 Rename DDL Script

--- src/main/resources/db/migration/V10__ProcessLog.sql
+++ src/main/resources/db/migration/V10__ProcessLog.sql
@@ -1,3 +1,4 @@
+-- Create table process_log used to store executions for migrations and results (start, completed, failed)
 CREATE TABLE process_log (
     process varchar(255) NOT NULL,
     event varchar(255) NOT NULL,
