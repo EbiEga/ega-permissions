92fc717bd88a1c5227adc275e1040200164d7d8b
Ashutosh <ashutosh@ebi.ac.uk>
2021-05-25 10:26:13 +0530

    Added feature to enable/disable cache

--- src/main/java/uk/ac/ebi/ega/permissions/configuration/CacheConfig.java
+++ src/main/java/uk/ac/ebi/ega/permissions/configuration/CacheConfig.java
@@ -18,11 +18,15 @@
 package uk.ac.ebi.ega.permissions.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -30,59 +34,93 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import uk.ac.ebi.ega.permissions.cache.CacheManager;
 import uk.ac.ebi.ega.permissions.cache.RedisCacheManager;
-import uk.ac.ebi.ega.permissions.cache.serializer.ValueRedisSerializer;
+import uk.ac.ebi.ega.permissions.cache.dto.DatasetDTO;
 import uk.ac.ebi.ega.permissions.cache.dto.UserDatasetPermissionDTO;
+import uk.ac.ebi.ega.permissions.cache.serializer.ValueRedisSerializer;
+
+import java.util.Set;
 
 import static com.fasterxml.jackson.databind.PropertyNamingStrategy.SNAKE_CASE;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.time.Duration.ofSeconds;
 import static org.springframework.data.redis.connection.jedis.JedisClientConfiguration.builder;
+import static uk.ac.ebi.ega.permissions.configuration.CacheConfig.NoCacheConfig;
+import static uk.ac.ebi.ega.permissions.configuration.CacheConfig.RedisCacheConfig;
 
+@Import({RedisCacheConfig.class, NoCacheConfig.class})
 @Configuration
 public class CacheConfig {
 
-    @ConfigurationProperties(prefix = "cache.redis")
-    @Bean
-    public RedisCacheConfigProperties initCacheRedisConfig() {
-        return new RedisCacheConfigProperties();
+    private static final Logger LOGGER = LoggerFactory.getLogger(CacheConfig.class);
+
+    @ConditionalOnProperty(name = "cache.redis.enable", havingValue = "true")
+    public static class RedisCacheConfig {
+        @ConfigurationProperties(prefix = "cache.redis")
+        @Bean
+        public RedisCacheConfigProperties initCacheRedisConfig() {
+            return new RedisCacheConfigProperties();
+        }
+
+        @Bean("jedisConnectionFactory")
+        public JedisConnectionFactory initJedisConnectionFactory(final RedisCacheConfigProperties properties) {
+            final RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+            redisStandaloneConfiguration.setHostName(properties.getHost());
+            redisStandaloneConfiguration.setPort(properties.getPort());
+
+            final JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration = builder();
+            jedisClientConfiguration.connectTimeout(ofSeconds(properties.getConnectionTimeout()));
+
+            return new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfiguration.build());
+        }
+
+        @Bean("redisTemplateCacheManager")
+        public RedisTemplate<String, UserDatasetPermissionDTO> initRedisTemplate(@Qualifier("jedisConnectionFactory") final JedisConnectionFactory factory,
+                                                                                 @Qualifier("cacheManagerObjectMapper") ObjectMapper objectMapper) {
+            final RedisTemplate<String, UserDatasetPermissionDTO> template = new RedisTemplate<>();
+            template.setKeySerializer(new StringRedisSerializer());
+            template.setValueSerializer(new ValueRedisSerializer(objectMapper, UTF_8));
+            template.setConnectionFactory(factory);
+            template.setEnableTransactionSupport(true);
+            return template;
+        }
+
+        @Bean
+        public CacheManager redisCacheManager(@Qualifier("redisTemplateCacheManager") final RedisTemplate<String, UserDatasetPermissionDTO> redisTemplate,
+                                              @Value("${cache.redis.namespace}") final String cacheNamespace) {
+            return new RedisCacheManager(
+                    redisTemplate,
+                    cacheNamespace
+            );
+        }
+
+        @Bean("cacheManagerObjectMapper")
+        public ObjectMapper objectMapper() {
+            final ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.setPropertyNamingStrategy(SNAKE_CASE);
+            return objectMapper;
+        }
     }
 
-    @Bean("jedisConnectionFactory")
-    public JedisConnectionFactory initJedisConnectionFactory(final RedisCacheConfigProperties properties) {
-        final RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(properties.getHost());
-        redisStandaloneConfiguration.setPort(properties.getPort());
+    @ConditionalOnProperty(name = "cache.redis.enable", havingValue = "false")
+    public static class NoCacheConfig {
+        @Bean
+        public CacheManager cacheManager() {
+            return new CacheManager() {
 
-        final JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration = builder();
-        jedisClientConfiguration.connectTimeout(ofSeconds(properties.getConnectionTimeout()));
+                @Override
+                public Set<DatasetDTO> addUserDatasetPermission(final String userAccountId,
+                                                                final DatasetDTO datasetDTO) {
+                    LOGGER.debug("No cache option has been activated; data won't be saved into cache.");
+                    return Set.of();
+                }
 
-        return new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfiguration.build());
-    }
-
-    @Bean("redisTemplateCacheManager")
-    public RedisTemplate<String, UserDatasetPermissionDTO> initRedisTemplate(@Qualifier("jedisConnectionFactory") final JedisConnectionFactory factory,
-                                                                             @Qualifier("cacheManagerObjectMapper") ObjectMapper objectMapper) {
-        final RedisTemplate<String, UserDatasetPermissionDTO> template = new RedisTemplate<>();
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new ValueRedisSerializer(objectMapper, UTF_8));
-        template.setConnectionFactory(factory);
-        template.setEnableTransactionSupport(true);
-        return template;
-    }
-
-    @Bean
-    public CacheManager redisCacheManager(@Qualifier("redisTemplateCacheManager") final RedisTemplate<String, UserDatasetPermissionDTO> redisTemplate,
-                                          @Value("${cache.redis.namespace}") final String cacheNamespace) {
-        return new RedisCacheManager(
-                redisTemplate,
-                cacheNamespace
-        );
-    }
-
-    @Bean("cacheManagerObjectMapper")
-    public ObjectMapper objectMapper() {
-        final ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setPropertyNamingStrategy(SNAKE_CASE);
-        return objectMapper;
+                @Override
+                public Set<DatasetDTO> deleteUserDatasetPermission(final String userAccountId,
+                                                                   final DatasetDTO datasetDTO) {
+                    LOGGER.debug("No cache option has been activated; data won't be deleted from cache.");
+                    return Set.of();
+                }
+            };
+        }
     }
 }
